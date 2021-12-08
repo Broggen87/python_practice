8c54d47b54e0c86ec6372f90cb4785cc61a501a2
Broggen87 <alex22459001@gmail.com>
2021-12-08 16:43:53 +0800

    update function practice code

--- function_1.py
+++ function_1.py
@@ -2,33 +2,23 @@ import os
 
 def read_file(file_name):
     data_1 = []
-    if os.path.isfile(file_name):
-        print('找到檔案，開啟中')
-        with open(file_name, 'r', encoding = 'utf-8') as f:
-            for i in f:
-                if '商品名稱,價格' in i:
-                    continue
-                name, price = i.strip().split(',')
-                data_1.append([name, price])
+    with open(file_name, 'r', encoding = 'utf-8') as f:
+        for i in f:
+            if '商品名稱,價格' in i:
+                continue
+            name, price = i.strip().split(',')
+            data_1.append([name, price])
         print(data_1)
-    else:
-        print('該檔案不存在')
     return(data_1)
 
-def items_input(products):
+def items_input():
     sheet = []
-    with open(products, 'w', encoding = 'utf-8') as f_1:
-        name = '商品名稱'
-        price = '價格'
+    while True:
+        name = input('請輸入產品名稱: ')
+        if name == 'end':
+            break
+        price = input('請輸入產品金額: ')
         sheet.append([name, price])
-        while True:
-            name = input('請輸入產品名稱: ')
-            if name == 'end':
-                break
-            price = input('請輸入產品金額: ')
-            sheet.append([name, price])
-        for i in sheet:
-            f_1.write(i[0] + ',' + i[1] + '\n')
     return(sheet)
 
 def print_data(w):
@@ -37,9 +27,24 @@ def print_data(w):
             continue
         print(a[0], '花費', a[1], '元')
 
-result = read_file(file_name = input('檔案名稱: '))
-sheet = items_input(input('建立檔案名稱: '))
-print_data(sheet)
+def creat_data(products, sheet):
+    with open(products, 'w', encoding = 'utf-8') as f_1:
+        f_1.write('商品名稱,價格\n')
+        for i in sheet:
+            f_1.write(i[0] + ',' + i[1] + '\n')
+
+
+file_name = input('檔案名稱: ')
+if os.path.isfile(file_name):
+    print('找到檔案，開啟中')
+    result = read_file(file_name)
+else:
+    print('該檔案不存在')
+
+sheet = items_input()
+print(sheet)
+creat_data(file_name, sheet)
+
 
 
 
